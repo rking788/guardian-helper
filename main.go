77c972af228da8d4207545268fa29d54aeaabf09
Rob King <rking188@gmail.com>
2017-05-03 00:32:26 -0400

    Now using the alexa skillserver package instead of a custom http router. No longer serving the privacy.html file from this app.

--- main.go
+++ main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"net/http"
 	"net/http/httputil"
 	"os"
 
@@ -13,64 +10,37 @@ import (
 	"github.com/mikeflynn/go-alexa/skillserver"
 )
 
+// Applications is a definition of the Alexa applications running on this server.
+var Applications = map[string]interface{}{
+	"/echo/guardian-helper": skillserver.EchoApplication{ // Route
+		AppID:    os.Getenv("ALEXA_APP_ID"), // Echo App ID from Amazon Dashboard
+		OnIntent: EchoIntentHandler,
+		OnLaunch: EchoIntentHandler,
+	},
+}
+
 func main() {
 
 	port := os.Getenv("PORT")
 
-	router := gin.New()
-	router.Use(gin.Logger())
-
-	router.GET("/", RootHandler)
-	router.StaticFile("/privacy.html", "./alexa/privacy.html")
-
-	echo := router.Group("/echo")
-	{
-		echo.POST("/guardian-helper", EchoIntentHandler)
-	}
-
 	fmt.Println(fmt.Sprintf("Start listening on port(%s)", port))
-	router.Run(":" + port)
-}
-
-// RootHandler is responsible for just responding with a String... that is all
-// for now.
-func RootHandler(ctx *gin.Context) {
-	ctx.String(http.StatusOK, "Welcome!\n")
+	skillserver.Run(Applications, port)
 }
 
 // Alexa skill related functions
 
 // EchoIntentHandler is a handler method that is responsible for receiving the
 // call from a Alexa command and returning the correct speech or cards.
-func EchoIntentHandler(ctx *gin.Context) {
+func EchoIntentHandler(echoRequest *skillserver.EchoRequest, echoResponse *skillserver.EchoResponse) {
 
-	requestBytes, err := ioutil.ReadAll(ctx.Request.Body)
-	if err != nil {
-		fmt.Println("Error reading echo request!: ", err.Error())
-		ctx.AbortWithError(http.StatusInternalServerError, err)
-		return
-	}
-
-	echoRequest := &skillserver.EchoRequest{}
-	json.Unmarshal(requestBytes, echoRequest)
-
-	var response *skillserver.EchoResponse
 	if echoRequest.GetIntentName() == "CountItem" {
-		response = alexa.CountItem(echoRequest)
+		echoResponse = alexa.CountItem(echoRequest)
 	} else if echoRequest.GetIntentName() == "TransferItem" {
-		response = alexa.TransferItem(echoRequest)
+		echoResponse = alexa.TransferItem(echoRequest)
 	} else {
-		response = skillserver.NewEchoResponse()
-		response.OutputSpeech("Sorry Guardian, I did not understand your request.")
+		echoResponse = skillserver.NewEchoResponse()
+		echoResponse.OutputSpeech("Sorry Guardian, I did not understand your request.")
 	}
-
-	bytes, err := response.String()
-	if err != nil {
-		fmt.Println("Failed to convert EchoResponse to a byte slice.")
-		return
-	}
-
-	ctx.String(http.StatusOK, string(bytes))
 }
 
 func dumpRequest(ctx *gin.Context) {
