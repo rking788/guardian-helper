f6f3aa979135de02b1bc0bdacd82679cc20532c3
Rob <rking188@gmail.com>
2017-05-08 22:11:33 -0400

    Added redis and now supporting sessions and LaunchIntents to leave a session open and prompt the user for some input.

--- alexa/alexa.go
+++ alexa/alexa.go
@@ -1,15 +1,112 @@
 package alexa
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 	"strconv"
+	"time"
 
 	"bitbucket.org/rking788/guardian-helper/bungie"
 
 	"strings"
 
+	"github.com/garyburd/redigo/redis"
 	"github.com/rking788/go-alexa/skillserver"
 )
+
+// Session is responsible for storing information related to a specific skill invocation.
+// A session will remain open if the LaunchRequest was received.
+type Session struct {
+	ID                   string
+	Action               string
+	ItemName             string
+	DestinationClassHash int
+	SourceClassHash      int
+	Quantity             int
+}
+
+var (
+	redisConnPool = newRedisPool(os.Getenv("REDIS_URL"))
+)
+
+// Redis related functions
+
+func newRedisPool(addr string) *redis.Pool {
+	// 25 is the maximum number of active connections for the Heroku Redis free tier
+	return &redis.Pool{
+		MaxIdle:     3,
+		MaxActive:   25,
+		IdleTimeout: 240 * time.Second,
+		Dial:        func() (redis.Conn, error) { return redis.DialURL(addr) },
+	}
+}
+
+// GetSession will attempt to read a session from the cache, if an existing one is not found, an empty session
+// will be created with the specified sessionID.
+func GetSession(sessionID string) (session *Session) {
+	session = &Session{ID: sessionID}
+
+	conn := redisConnPool.Get()
+	defer conn.Close()
+
+	key := fmt.Sprintf("sessions:%s", sessionID)
+	reply, err := redis.String(conn.Do("GET", key))
+	if err != nil {
+		fmt.Println("Failed to read session from cache:", err.Error())
+		return
+	}
+
+	err = json.Unmarshal([]byte(reply), session)
+
+	return
+}
+
+// SaveSession will persist the given session to the cache. This will allow support for long running
+// Alexa sessions that continually prompt the user for more information.
+func SaveSession(session *Session) {
+
+	conn := redisConnPool.Get()
+	defer conn.Close()
+
+	sessionBytes, err := json.Marshal(session)
+	if err != nil {
+		fmt.Println("Couldn't marshal session to string: ", err.Error())
+		return
+	}
+
+	key := fmt.Sprintf("sessions:%s", session.ID)
+	_, err = conn.Do("SET", key, string(sessionBytes))
+	if err != nil {
+		fmt.Println("Failed to set session: ", err.Error())
+	}
+}
+
+// ClearSession will remove the specified session from the local cache, this will be done
+// when the user completes a full request session.
+func ClearSession(sessionID string) {
+
+	conn := redisConnPool.Get()
+	defer conn.Close()
+
+	key := fmt.Sprintf("sessions:%s", sessionID)
+	_, err := conn.Do("DEL", key)
+	if err != nil {
+		fmt.Println("Failed to delete the session from the Redis cache: ", err.Error())
+	}
+}
+
+// WelcomePrompt is responsible for prompting the user with information about what they can ask
+// the skill to do.
+func WelcomePrompt(echoRequest *skillserver.EchoRequest) (response *skillserver.EchoResponse) {
+	response = skillserver.NewEchoResponse()
+
+	response.OutputSpeech("Welcome Guardian, would you like to transfer an item to a specific character or find out how many of an item you have?").
+		Reprompt("Do you want to transfer an item or find out how much of an item you have?").
+		EndSession(false)
+
+	return
+}
 
 // CountItem calls the Bungie API to see count the number of Items on all characters and
 // in the vault.
